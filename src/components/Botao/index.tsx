e90d734b09cfdaf5d8a993733de19a182bdf3589
Melksedeque <freelancer@melksedeque.com.br>
2025-04-23 13:03:19 -0300

    Background de teste do botão

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -3,7 +3,11 @@ import React from "react";
 class Botao extends React.Component {
   render() {
     return (
-      <button className="" onClick={() => alert("Botão clicado!")}>
+      <button
+        className=""
+        onClick={() => alert("Botão clicado!")}
+        style={{ backgroundColor: "blue" }}
+      >
         {/* {this.props.children} */}
         Salvar
       </button>
