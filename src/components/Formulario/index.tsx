4eeb164bc8c97a6e04376ce9bf642fbe3bd02545
Melksedeque <freelancer@melksedeque.com.br>
2025-04-23 13:30:37 -0300

    Modularização e nomenclatura dos estilos CSS dentro dos componentes

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Botao from "../Botao";
-import style from "./style.module.scss";
+import style from "./Formulario.module.scss";
 
 class Formulario extends React.Component {
   render() {
