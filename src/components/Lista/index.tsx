4f9666ad729d7802fc303c01d06c994051aa5b3f
Melksedeque <freelancer@melksedeque.com.br>
2025-04-24 15:03:32 -0300

    useState para manipular tarefas iniciais

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import Item from "./Item";
 import style from "./Lista.module.scss";
 
 function Lista() {
-  const tarefas = [
+  const [tarefas, setTarefas] = useState([
     { tarefa: "React", tempo: "02:00:00" },
     { tarefa: "Javascript", tempo: "01:00:00" },
     { tarefa: "Typescript", tempo: "03:00:00" },
-  ];
+  ]);
   return (
     <aside className={style.listaTarefas}>
-      <h2>Estudos do dia</h2>
+      <h2
+        onClick={() => {
+          setTarefas([...tarefas, { tarefa: "Angular", tempo: "04:00:00" }]);
+        }}
+      >
+        Estudos do dia
+      </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item key={index} {...item} />
