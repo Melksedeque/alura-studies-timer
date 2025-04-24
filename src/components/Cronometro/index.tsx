1a368bd78f00a11d832025e0cb9b4311c52dc2eb
Melksedeque <freelancer@melksedeque.com.br>
2025-04-24 14:47:22 -0300

    Aplicação de classes CSS no estilo do Cronometro

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -5,8 +5,8 @@ import style from "./Cronometro.module.scss";
 export default function Cronometro() {
   return (
     <div className={style.cronometro}>
-      <p>Escolha um card e inicie o cronômetro</p>
-      <div>
+      <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      <div className={style.relogioWrapper}>
         <Relogio />
       </div>
       <Botao>Começar</Botao>
