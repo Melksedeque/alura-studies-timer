13edbccf901844c733a4711ca57736ef45e895c3
Melksedeque <freelancer@melksedeque.com.br>
2025-04-25 17:20:39 -0300

    Tipagem de useState de Tarefas podendo receber um array de ITarefas ou um array vazio

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,9 +3,10 @@ import Cronometro from "../components/Cronometro";
 import Formulario from "../components/Formulario";
 import Lista from "../components/Lista";
 import style from "./App.module.scss";
+import { ITarefa } from "../types/tarefa";
 
 function App() {
-  const [tarefas, setTarefas] = useState([]);
+  const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
