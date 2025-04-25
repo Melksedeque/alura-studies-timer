6ac17422ba8e2ee980b062f04d626f6a28263ab2
Melksedeque <freelancer@melksedeque.com.br>
2025-04-25 17:18:18 -0300

    Remoção de alert() de teste do botão

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,17 +2,13 @@ import React from "react";
 import style from "./Botao.module.scss";
 
 class Botao extends React.Component<{
-  type?: "button" | "submit" | "reset" | undefined,
+  type?: "button" | "submit" | "reset" | undefined;
   children: React.ReactNode;
 }> {
   render() {
     const { type = "button" } = this.props;
     return (
-      <button
-        type={type}
-        className={style.botao}
-        onClick={() => alert("Botão clicado!")}
-      >
+      <button type={type} className={style.botao}>
         {this.props.children}
       </button>
     );
