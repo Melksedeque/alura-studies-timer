ce32dbfcb378fd4ee379e903fd5b77db1e78b899
Melksedeque <freelancer@melksedeque.com.br>
2025-04-29 12:29:34 -0300

    Criação de interface Props para passar função de selecionar Tarefa para os itens da lista

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,15 +1,27 @@
 import { ITarefa } from "../../../types/tarefa";
 import style from "../Lista.module.scss";
 
+interface Props extends ITarefa  {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
 export default function Item({
   tarefa,
   tempo,
   selecionado,
   completado,
   id,
-}: ITarefa) {
+  selecionaTarefa
+}: Props) {
+  console.log("Item atual: ", {id, tarefa, tempo, selecionado, completado})
   return (
-    <li className={style.item}>
+    <li className={style.item} onClick={() => selecionaTarefa({
+      tarefa,
+      tempo,
+      selecionado,
+      completado,
+      id,
+    })}>
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
