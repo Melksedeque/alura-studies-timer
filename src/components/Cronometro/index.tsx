3b2b2d10d891c67ddd11c4ed3749da6a3bf913ab
Melksedeque <freelancer@melksedeque.com.br>
2025-04-29 15:53:38 -0300

    função inicial para capturar o tempo do item selecionado {erro}

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -2,12 +2,21 @@ import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss";
 import { tempoParaSegundos } from "../../common/utils/datetime";
+import { ITarefa } from "../../types/tarefa";
+import { useState } from "react";
 
-export default function Cronometro() {
-  console.log("Conversão: ", tempoParaSegundos("01:01:01"));
+interface Props {
+  selecionado: ITarefa | undefined;
+}
+
+export default function Cronometro({selecionado}: Props) {
+  const [tempo, setTempo] = useState(tempoParaSegundos(selecionado?.tempo ?? "00:00:00"));
+  const [iniciado, setIniciado] = useState(false);
+  const [intervalo, setIntervalo] = useState<number | undefined>(undefined);
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      Tempo: {tempo}
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
