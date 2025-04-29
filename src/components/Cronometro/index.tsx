dd9bcd3d8874ec5571456017ce2f66e65fee1cec
Melksedeque <freelancer@melksedeque.com.br>
2025-04-29 15:54:13 -0300

    Correção da função de capturar o tempo do item selecionado; uso do useEffect para renderizar o componente e validar o tempo

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -3,16 +3,21 @@ import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss";
 import { tempoParaSegundos } from "../../common/utils/datetime";
 import { ITarefa } from "../../types/tarefa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
   selecionado: ITarefa | undefined;
 }
 
 export default function Cronometro({selecionado}: Props) {
-  const [tempo, setTempo] = useState(tempoParaSegundos(selecionado?.tempo ?? "00:00:00"));
-  const [iniciado, setIniciado] = useState(false);
-  const [intervalo, setIntervalo] = useState<number | undefined>(undefined);
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado]);
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
