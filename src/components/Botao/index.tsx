bc6936cdf8bbfb26b40de6575f1a201fecbef2c0
Melksedeque <freelancer@melksedeque.com.br>
2025-04-30 19:04:55 -0300

    Adição de onClick na tipagem do botão

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import style from "./Botao.module.scss";
 
 class Botao extends React.Component<{
-  type?: "button" | "submit" | "reset" | undefined;
-  children: React.ReactNode;
+  type?: "button" | "submit" | "reset" | undefined,
+  children: React.ReactNode,
+  onClick?: () => void,
 }> {
   render() {
-    const { type = "button" } = this.props;
+    const { type = "button", onClick } = this.props;
     return (
-      <button type={type} className={style.botao}>
+      <button type={type} className={style.botao} onClick={onClick}>
         {this.props.children}
       </button>
     );
